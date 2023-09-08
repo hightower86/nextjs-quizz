1143c627a1e886e43f7b61c70be133abe7dcf7d1
arezvanov@arezvanov <adel.rezvanov@noveogroup.com>
2023-09-08 09:43:02 +0400

    Clerk auth added with base setup

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,11 +1,12 @@
 import { FC } from 'react';
-
+import { UserButton } from '@clerk/nextjs';
 interface LandingPageProps {}
 
 const LandingPage: FC<LandingPageProps> = ({}) => {
     return (
-        <div>
+        <div className="flex justify-between gap-4 px-8 py-4">
             <h1 className="text-7xl">LandingPage</h1>
+            <UserButton afterSignOutUrl="/" />
         </div>
     );
 };
